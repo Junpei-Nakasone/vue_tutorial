ce660ade7b4fffb6beeeb2691c3c660731fbc7de
Junpei-Nakasone <nakasone.junpei0719@gmail.com>
2020-06-02 06:23:16 +0900

    modified

--- vue_tutorial/src/App.vue
+++ vue_tutorial/src/App.vue
@@ -1,17 +1,18 @@
 <template>
-<p>Like({{number}})</p>
-  
+<div>
+<LikeHeader></LikeHeader>
+<LikeNumber></LikeNumber>
+</div>
+
 </template>
 
 <script>
+import LikeHeader from "./LikeHeader.vue"
 export default {
-    data: function() {
-        return {
-            number: 5
-        }
+    components: {
+        LikeHeader
     }
 }
-
 </script>
 
 <style>
---
+++ vue_tutorial/src/LikeHeader.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>LIKE</h1>
+</template>
